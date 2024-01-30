70746f92d3eeccd51cd6b4573be6e7d9a86fb4d9
RachBartmoss <julesleveillard@gmail.com>
2024-01-30 23:12:54 +0100

    Stub implementation of Gamestate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::all, clippy::pedantic)]
+
+use bracket_lib::prelude::*;
+
+enum Gamemode {
+    Menu,
+    Play,
+    Pause,
+}
+
+struct State {
+    mode: Gamemode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: Gamemode::Menu,
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {}
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Practice Game")
+        .build()?;
+
+    main_loop(context, State::new())
 }
