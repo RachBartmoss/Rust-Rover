70324a0d229116f43f43233d3e768cd5cb88654d
RachBartmoss <julesleveillard@gmail.com>
2024-01-31 11:57:47 +0100

    Skeleton implementation of gamemodes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #![warn(clippy::all, clippy::pedantic)]
 
+use std::alloc::GlobalAlloc;
+
 use bracket_lib::prelude::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
 
 enum Gamemode {
     Menu,
     Play,
     Pause,
+    Quit,
 }
 
 struct State {
@@ -18,10 +24,33 @@ impl State {
             mode: Gamemode::Menu,
         }
     }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        todo!();
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        todo!();
+    }
+
+    fn pause(&mut self, ctx: &mut BTerm) {
+        todo!();
+    }
+
+    fn quit(&mut self, ctx: &mut BTerm) {
+        todo!();
+    }
 }
 
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {}
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            Gamemode::Menu => self.main_menu(ctx),
+            Gamemode::Play => self.play(ctx),
+            Gamemode::Pause => self.pause(ctx),
+            Gamemode::Quit => self.quit(ctx),
+        }
+    }
 }
 
 fn main() -> BError {
